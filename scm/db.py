0ec2db89ce7c7470d3acc0288a74f86282d6c1b3
Stahash <stakhanova.anastasia@gmail.com>
2018-02-25 22:25:42 +0300

    enchasemnts web and refactoring

--- scm/db.py
+++ scm/db.py
@@ -13,10 +13,12 @@ class ReceiptDb():
             self.receipts.create_index('title', 'text', unique=True)
             self.receipts.create_index('description', 'text')
             self.receipts.create_index('ingredients', 'text')
+            self.receipts.create_index('invalid')
+            self.receipts.create_index('recipe_id', unique=True)
 
-    def save_receipt(self, receipt):
-        return self.receipts.update_one({'title': receipt['title']},
-                                 {'$set': receipt}, upsert=True)
+    def save_recipe(self, recipe):
+        return self.receipts.update_one({'title': recipe['title']},
+                                        {'$set': recipe}, upsert=True)
 
     def get_receipt(self, receipt_title):
         return self.receipts.find_one({'title': receipt_title})
@@ -30,5 +32,13 @@ class ReceiptDb():
 
     def clean_recipes(self):
         return self.receipts.delete_many({})
+
+    def get_invalid_recipe_ids(self):
+        return set(map(lambda x: x.get('recipe_id'),
+                       self.receipts.find({'invalid': True}, {'recipe_id': True})))
+
+    def get_all_recipes(self):
+        return self.receipts.find()
+
 
 receipt_db = ReceiptDb()
--- scm/server.py
+++ scm/server.py
@@ -32,16 +32,22 @@ def process_ingredients(ingredients_str):
 
 def parse_ingredients(input):
     return map(
-        lambda x: {'name': x[:x.index('-')].strip(), 'value': x[x.index('-'):].strip()},
-        input.split(',')
+        lambda x: {'name': x[:x.index('-')].strip(), 'value': x[x.index('-') + 1:].strip()},
+        filter(lambda x: x, input.split(','))
     )
 
 
 @app.route("/")
 def recipes_ok():
     q = request.args.get("q", None)
-    recipe_list = data_base.get_receipt_like(q)
-    return render_template("main.html", recipes=recipe_list)
+    if q:
+        recipe_list = data_base.get_receipt_like(q)
+    else:
+        recipe_list = list(data_base.get_all_recipes())
+
+    bad_recipe_ids = data_base.get_invalid_recipe_ids()
+
+    return render_template("main.html", recipes=recipe_list, bad_receipe_ids=bad_recipe_ids)
 
 
 @app.route("/update_recipe", methods=['POST'])
@@ -55,9 +61,11 @@ def update_recipe():
 
     description = request.form.get('description')
     decorating = request.form.get('decorating')
+    recipe_id = request.form.get('recipe_id')
     if title and ingredients and description:
-        save_result = data_base.save_receipt(
-            {'title': title, 'description': description, 'ingredients': ingredients, 'decorating': decorating})
+        save_result = data_base.save_recipe(
+            {'title': title, 'description': description, 'ingredients': ingredients, 'decorating': decorating,
+             'recipe_id': recipe_id})
         return jsonify(**{'ok': True, 'updated': save_result.modified_count})
 
     return jsonify(**{'ok': False, 'error': 'Needed some fields value.'})
@@ -68,22 +76,23 @@ def upload_recipes():
     # check if the post request has the file part
     if 'cocktails_file' not in request.files:
         flash('No file part')
-        return redirect(request.url)
+        return redirect(url_for('recipes_ok'))
     file = request.files['cocktails_file']
     # if user does not select file, browser also
     # submit a empty part without filename
     if file.filename == '':
         flash('No selected file')
-        return redirect(request.url)
+        return redirect(url_for('recipes_ok'))
     if file and file.filename:
         bad_recipes = []
         try:
             for recipe in load(file):
-                if recipe_is_valid(recipe):
-                    data_base.save_receipt(recipe)
-                else:
-                    bad_recipes.append(recipe)
-            return render_template("main.html", bad_recipes=bad_recipes)
+                if not recipe_is_valid(recipe):
+                    recipe['invalid'] = True
+
+                data_base.save_recipe(recipe)
+
+            return redirect(url_for('recipes_ok'))
         except Exception as e:
             print e
             flash('Some error: %s' % e.message)
