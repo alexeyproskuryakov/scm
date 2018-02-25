d4bd47719eb01ef5f9453206240b926e43fc557e
Stahash <stakhanova.anastasia@gmail.com>
2018-02-25 21:16:22 +0300

    Recipes edit

--- scm/loader.py
+++ scm/loader.py
@@ -1,36 +1,36 @@
 # coding=utf-8
 import re
-from scm.db import ReceiptDb, receipt_db
+from scm.db import receipt_db
 
 main_re = re.compile(
     u'^(\d+ )?([a-zA-Z0-9а-яёА-Я -`]+) ?\(([a-zA-Zа-яёА-Я0-9—, -]+(\(.+\))?[a-zA-Zа-яёА-Я0-9—, -]+)[.)](.*)\)?$')
 
 
+def load(file):
+    counter = 0
+    err = 0
+    for line in file.readlines():
+        # print line
+        line = line.decode('utf-8')
+        if '(' in line:
+            q_ind = line.index('(')
+            if q_ind:
+                title = line[:q_ind]
+                existed = receipt_db.get_receipt(title)
+                if existed: continue
+                description = line[q_ind:]
+                ingredients = []
+                if title and description:
+                    print 'title %s\ningrs: %s\ndescr: %s' % (title, ingredients, description)
+                    yield {'title': title, 'description': description, 'ingredients': ingredients,
+                           'recipe_id': hash('%s%s%s' % (title, description, ingredients))}
+        else:
+            print 'ERROR :('
+            err += 1
+        counter += 1
 
-
-def load():
-    with open('../cocktails') as f:
-        counter = 0
-        err = 0
-        for line in f.readlines():
-            # print line
-            line = line.decode('utf-8')
-            if '(' in line:
-                q_ind = line.index('(')
-                if q_ind:
-                    title = line[:q_ind]
-                    description = line[q_ind:]
-                    ingredients = []
-                    if title  and description:
-                        print 'title %s\ningrs: %s\ndescr: %s' % (title, ingredients, description)
-                        receipt_db.save_receipt({'title': title, 'description': description, 'ingredients': ingredients})
-            else:
-                print 'ERROR :('
-                err += 1
-            counter += 1
-
-            print '-------------------%s'%counter
-        print counter, err
+        print '-------------------%s' % counter
+    print counter, err
 
 
 if __name__ == '__main__':
--- scm/server.py
+++ scm/server.py
@@ -1,23 +1,102 @@
-from flask import Flask, request, render_template, jsonify
+import os
+from flask import Flask, request, render_template, jsonify, flash, url_for
+from werkzeug.utils import redirect, secure_filename
 
 from scm.db import ReceiptDb
-
+from scm.loader import load
 
 app = Flask(__name__, template_folder='templates', static_folder='static')
+
+UPLOAD_FOLDER = '/path/to/the/uploads'
+ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'}
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
+app.secret_key = 'super secret key'
+app.config['SESSION_TYPE'] = 'filesystem'
+
 data_base = ReceiptDb()
 
-@app.route("/", methods = ["POST", "GET"])
-def recipes_management():
-    if request.method == "GET":
-        q = request.args.get("q", None)
-        recipe_list = data_base.get_receipt_like(q)
-        return render_template("main.html", recipes = recipe_list)
-    return jsonify(**{'ok':True})
+
+def recipe_is_valid(recipe):
+    return recipe.get('title') and recipe.get('ingredients') and recipe.get('description')
+
+
+def process_ingredients(ingredients_str):
+    return dict(
+        map(
+            lambda x: (x.split('=')[0], x.split('=')[1]),
+            ingredients_str.split('|')
+        )
+    )
+
+
+def parse_ingredients(input):
+    return map(
+        lambda x: {'name': x[:x.index('-')].strip(), 'value': x[x.index('-'):].strip()},
+        input.split(',')
+    )
+
+
+@app.route("/")
+def recipes_ok():
+    q = request.args.get("q", None)
+    recipe_list = data_base.get_receipt_like(q)
+    return render_template("main.html", recipes=recipe_list)
+
+
+@app.route("/update_recipe", methods=['POST'])
+def update_recipe():
+    title = request.form.get('title')
+    try:
+        ingredients_value = request.form.get('ingredients')
+        ingredients = parse_ingredients(ingredients_value)
+    except Exception:
+        return jsonify(**{'ok': False, 'error': 'Ingredients field value %s is not valid' % ingredients_value})
+
+    description = request.form.get('description')
+    decorating = request.form.get('decorating')
+    if title and ingredients and description:
+        save_result = data_base.save_receipt(
+            {'title': title, 'description': description, 'ingredients': ingredients, 'decorating': decorating})
+        return jsonify(**{'ok': True, 'updated': save_result.modified_count})
+
+    return jsonify(**{'ok': False, 'error': 'Needed some fields value.'})
+
+
+@app.route('/upload_recipes', methods=['POST'])
+def upload_recipes():
+    # check if the post request has the file part
+    if 'cocktails_file' not in request.files:
+        flash('No file part')
+        return redirect(request.url)
+    file = request.files['cocktails_file']
+    # if user does not select file, browser also
+    # submit a empty part without filename
+    if file.filename == '':
+        flash('No selected file')
+        return redirect(request.url)
+    if file and file.filename:
+        bad_recipes = []
+        try:
+            for recipe in load(file):
+                if recipe_is_valid(recipe):
+                    data_base.save_receipt(recipe)
+                else:
+                    bad_recipes.append(recipe)
+            return render_template("main.html", bad_recipes=bad_recipes)
+        except Exception as e:
+            print e
+            flash('Some error: %s' % e.message)
+            return redirect(url_for('recipes_ok'))
+
 
 @app.route("/clean")
 def clean_recipes_list():
     result = data_base.clean_recipes()
-    return jsonify(**{'ok':result.deleted_count != 0})
+    return jsonify(**{'ok': result.deleted_count != 0})
+
 
 if __name__ == '__main__':
-    app.run(port=7777, host='0.0.0.0')
+    app.config['TEMPLATES_AUTO_RELOAD'] = True
+    app.jinja_env.auto_reload = True
+    app.run(port=7777, debug=True)
